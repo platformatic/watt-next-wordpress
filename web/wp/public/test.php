dc416fee531603a2769d4e64d4c778949796f255
Matteo Collina <hello@matteocollina.com>
2025-07-07 17:26:46 +0200

    Added dist/ to make Wordpress editor work (#10)
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- web/wp/public/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// If behind a reverse proxy that sets custom headers
-$protocol = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? (isset($_SERVER['HTTPS']) ? 'https' : 'http');
-$host = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? $_SERVER['HTTP_HOST'];
-
-echo $_SERVER['HTTP_X_FORWARDED_PROTO'] . "\n";
-echo $_SERVER['HTTP_X_FORWARDED_HOST'] . "\n";
-?>
